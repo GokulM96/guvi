c10e31223586e0bf3cf670052746aba37aa16acb
GokulM96 <gokulms1996@gmail.com>
2017-06-09 15:33:31 +0530

    Create Alphabet.c

---
+++ Alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ char b;
+ scanf("%d",&b);
+ if(b=='a' && b=='z') || (b=='A' && b=='Z')
+ printf("The number is alphabet");
+ else
+ printf("the number is not an alphabet");
+ return 0;
+ }
