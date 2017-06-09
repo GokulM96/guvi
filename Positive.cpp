fbc1ae625426eb57e5f60bc64a68eac6d770c4e7
GokulM96 <gokulms1996@gmail.com>
2017-06-09 14:02:17 +0530

    Positive.cpp
    cpp program

---
+++ Positive.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int number;
+cin>>number;
+if(number>0)
+{
+cout<<"the number is positive";
+}
+else if(number<0)
+{
+cout<<"the number is negative";
+}
+else
+cout<<"the number is zero";
+getch();
+}
