7192a88c03217798de3a0c8abd29e8f0418ddcb4
GokulKce <gokulms1996@gmail.com>
2017-06-10 09:33:24 +0530

    Create Leap year or not.c

---
+++ Leap year or not.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int y;
+scanf("%d",&y);
+if(y % 4 == 0)
+if(y % 100 == 0)
+if(y % 400 == 0)
+printf("It is a leap year");
+else
+printf("It is not a leap year");
+else
+printf("It is a leap year");
+else
+printf("It is a leap year");
+return 0;
+}
